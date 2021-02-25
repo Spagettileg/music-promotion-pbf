e03a9deee1d5ee34389c3fd7905a2c397d5fc313
Ubuntu <ubuntu@ip-172-31-32-134.eu-west-1.compute.internal>
2021-02-25 23:45:01 +0000

    New return to top button function added. User will see buttton appear through scrolling down page by minimum of 575px.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,21 @@ $(document).ready(function () {
     $('#performing-years').text('1966 - ' + new Date().getFullYear());
     
 });
+
+// Scroll to top of webpage...
+
+    $("a[href='#move-up']").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+
+    // If position of vertical scroll is above 575px, for 
+    // return to top button to disappear.
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 575) {
+            $('.move-top').fadeIn();
+        } else {
+            $('.move-top').fadeOut();
+        }
+    });
